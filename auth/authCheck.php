560376cee64190ef4e49398ed21d2d3f49075c43
Andrey <glushok19999@gmail.com>
2023-08-14 15:42:08 +0300

    Добавил ip

--- auth/authCheck.php
+++ auth/authCheck.php
@@ -6,7 +6,7 @@ $minimaster = false;
 
 if (
     //$_SERVER["REMOTE_ADDR"] == '5.39.162.100' ||
-    $_SERVER["REMOTE_ADDR"] == '31.173.10.9' ||
+    $_SERVER["REMOTE_ADDR"] == '31.173.10.9'
    // $_SERVER["REMOTE_ADDR"] == '91.79.60.32' ||
     //$_SERVER["REMOTE_ADDR"] == '176.108.160.115' ||
     //$_SERVER["REMOTE_ADDR"] == '93.80.171.13' ||
