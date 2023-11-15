cfcf7977ec735aaa6813a0a86ec0c1218ba0927f
glushok <glushok19999@gmail.com>
2023-11-15 11:00:14 +0300

    правки

--- game15.php
+++ game15.php
@@ -1,6 +1,10 @@
 <?
     require_once $_SERVER['DOCUMENT_ROOT'] . '/base/connect.php';
 
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
 	$kod = strtoupper(base_convert(random_int(4369,65535), 10, 16));
 	$NewDate = Date('Y-m-d H:i:s', strtotime('+2 days'));
 	$stmt = $dbh->prepare("INSERT INTO promo_kods(series_id, sale_procent, active, date_end) values ('" . $kod . "', '10', '1', '" . $NewDate . "')");
