37305fc19ea4d0d9c9404a694ae8a54c25c3a5ee
glushok <glushok19999@gmail.com>
2025-02-24 19:10:00 +0300

    правки

--- formation_football.php
+++ formation_football.php
@@ -1,64 +1,274 @@
 <?php
-$keywords = "Оформление футболок, рамка для футболки, панно для";
-$title = "Панно для спортивных футболок в Багетной мастерской №1!";
-$description = "Профессиональное оформление футболок, кофт и других атрибутов в багетные рамы: сроки, стоимость и гарантии от команды Багетной мастерской №1";
+$keywords = "Оформление футболок, рамка для футболки, объемное оформление, джерси";
+$title = "Оформление футболок и джерси в Багетной мастерской №1!";
+$description = "Объемное оформление джерси, футболок и других спортивных атрибутов в рамы: сроки, стоимость и гарантии от команды Багетной мастерской №1";
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 ?>
-    <div class="container text-center">
-        <h1>Оформление футболок</h1>
-        <hr>
+    <div class="container">
+        <div class="row text-center mt-3 mb-5">
+            <h1 class="color-main block-h1">ПАННО ДЛЯ ФУТБОЛОК И ДЖЕРСИ В БАГЕТНОЙ МАСТЕРСКОЙ №1</h1>
+        </div>
 
-        <div id="main">
-            <h1>Оформление футболок и других атрибутов в Багетной мастерской №1 ?</h1>
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-4 text-center hide-mobile">
+                <img src="/img/article/IMG_0901.jpeg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
 
-            <img src="/img/photo_2023-05-19_12-28-31.jpg" align="left" width="200px">
-            <p>
-                Кому доверить изготовление рамки для футболки? Нередко подобные рамки изготавливаются для ценных своему
-                обладателю вещей: раритетные и трофейные шайбы, футболки с автографами, хоккейные клюшки и кофты любимой
-                команды. Такие вещи требуют особой заботы: для их сохранности создаются специальные ниши или панно.
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
+                    Спортивные футболки, особенно джерси, — это не просто предмет гардероба, а настоящие памятные вещи,
+                    которые хранят в себе историю побед, эмоции и воспоминания. Чтобы сохранить их в идеальном состоянии
+                    и превратить в стильный элемент интерьера, многие обращаются к нам для оформления футболок в рамы. В
+                    этой статье мы расскажем, как происходит процесс объемного оформления футболок и какие нюансы важно
+                    учитывать.
+                </p>
+            </div>
+        </div>
+
+        <div class="row text-center m-2">
+            <h3>ПОДГОТОВКА ДЖЕРСИ К ОФОРМЛЕНИЮ</h3>
+        </div>
+        <div class="row m-3 text-center">
+            <p style="font-weight: bold">
+                Перед тем как приступить к оформлению, футболку необходимо подготовить. Для этого:
             </p>
+            <p style="padding-bottom: 0; margin-bottom: 5px;">- Аккуратно постирайте и высушите изделие, чтобы удалить
+                все загрязнения. </p>
+            <p style="padding-bottom: 0; margin-bottom: 5px;">- Проверьте, нет ли на ткани повреждений или пятен,
+                которые могут испортить внешний вид.</p>
+            <p>- Отутюжьте или отпарьте изделие перед посещением мастерской для качественного результата работ</p>
+        </div>
 
-            <p>
-                Обширный опыт нашей команды позволяет создавать качественные и надежные спортивные панно, а также
-                непосредственно
-                располагать футболки внутри подготовленной конструкции. Больше готовых заказов с оформленными спортивными
-                атрибутами
-                смотрите <a href="https://bagetnaya-masterskaya.com/сatalog-of-finished-works.php">здесь</a>.
+        <div class="row text-center justify-content-center mt-3 mb-5">
+            <a href="/сatalog-of-finished-works">
+                <button
+                        class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
+                    Галерея работ</b></button>
+            </a>
+        </div>
+
+        <div class="row text-center m-2">
+            <h3>ВЫБОР РАМКИ ДЛЯ ФУТБОЛКИ</h3>
+        </div>
+        <div class="row m-2 text-center">
+            <p style="font-weight: bold">
+                Багетная рама — это основа всего оформления. В Багетной мастерской №1 Вам предложат несколько вариантов
+                оформления:
             </p>
+        </div>
 
-            <h5>Стоимость и сроки изготовления</h5>
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-8 my-auto">
+                <p>- Профиль «коробочка»: Поскольку джерси — это объемный предмет, рамка должна быть достаточно
+                    глубокой, чтобы ткань не прижималась к стеклу.</p>
+                <p>- Материал рамы: Дерево, металл или пластик — выбор зависит от ваших предпочтений и стиля
+                    интерьера. </p>
+                <p>- Цвет и дизайн: Не только рама должна подчеркивать цветовую гамму футболки и гармонировать с
+                    окружающим пространством. В салоне Багетной мастерской №1 Вам предложат большой выбор различных
+                    фонов, а также персонализированные постеры, фото, металлические таблички и многое другое.</p>
+            </div>
 
+            <div class="col-12 col-md-4 text-center">
+                <img src="/img/article/IMG_3390.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
 
-            <img src="/img/photo_2023-05-19_12-28-26.jpg" align="right" width="200px">
+            </div>
 
-            <p>
-                Панно для футболок – это небыстрый и кропотливый процесс. Сначала Вы полностью согласовываете макет будущего
-                оформления с
-                дизайнером. Наши специалисты могут подготовить для Вас схематичную визуализацию будущей готовой работы.
-                Расположение рукавов,
-                фотографии и декоративные таблички – все это проговаривается детально! После этого уже мастер снимает мерки
-                с
-                будущего внутри объекта и рассчитывается конечная стоимость работ. Если Вы планируете заказать оформление
-                футболки,
-                не ожидайте готовности за 2-3 дня. На подобные заказы не редко уходит до 10 календарных дней!
+        </div>
+
+        <div class="row text-center justify-content-center mt-3 mb-5">
+            <a href="/baget_online">
+                <button
+                        class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
+                    Рассчитать стоимость багета</b></button>
+            </a>
+        </div>
+
+        <div class="row text-center m-2">
+            <h3>3D ОФОРМЛЕНИЕ ФУТБОЛКИ</h3>
+        </div>
+        <div class="row m-3 text-center">
+            <p style="font-weight: bold">
+                Объемное оформление — это ключевой этап, который позволяет сохранить естественную форму джерси. Для
+                этого используются специальные техники:
             </p>
+            <p><span style="color: #AD1F2D;">- Монтажная подложка:</span> Футболка аккуратно растягивается на плотной
+                основе, чтобы избежать складок и неровностей. Такая техника используется в случае, если джерси нельзя
+                подшивать вручную. </p>
+            <p><span style="color: #AD1F2D;">- Фиксация нитями:</span>Ткань закрепляется с помощью ручной фиксации
+                нитями подходящего цвета, чтобы не повредить материал. Данный способ самый распространенный.</p>
+            <p><span style="color: #AD1F2D;">- Размещение футболки на «плечиках»:</span>еще один способ разместить
+                изделие в раме без жесткой фиксации</p>
+            <p style="color: #AD1F2D;">Важно! Мы не используем клей и клеевые материалы как способ фиксации тканевых
+                изделий!</p>
+        </div>
 
-            <p>
-                Стоимость подобного заказа рассчитывается строго индивидуально и зависит от множества параметров: размер
-                футболки,
-                основные материалы (рама, стекло, подложка), дополнительные материалы (фотографии, таблички, кантики и
-                прочее).
-            </p>
+        <div class="row text-center justify-content-center mt-3 mb-5">
 
-            <p>
-                «Багетная мастерская №1» гарантирует качественное и своевременное изготовление Вашего заказа: панно для
-                спортивных
-                атрибутов требует особого внимания и профессиональности, чем наша команда точно обладает!
+            <button
+                    class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'
+                    data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                Оставить заявку на обратную связь</b></button>
+
+        </div>
+
+        <div class="row text-center m-2">
+            <h3>ДОПОЛНИТЕЛЬНЫЕ ЭЛЕМЕНТЫ В РАМКАХ ДЛЯ ФУТБОЛКИ</h3>
+        </div>
+
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-4 text-center hide-mobile">
+                <img src="/img/article/IMG_2431.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p style="font-weight: bold">
+                    Чтобы сделать композицию более интересной, можно добавить:
+                </p>
+                <p><span style="color: #AD1F2D;">- Таблички с информацией:</span> Например, дата матча, имя игрока или
+                    значимое событие.</p>
+                <p><span style="color: #AD1F2D;">- Фотографии:</span>Снимки с игры или автографы.</p>
+                <p><span style="color: #AD1F2D;">- Аксессуары:</span>Медали, билеты на матч или другие памятные вещи.
+                </p>
+                <p><span style="color: #AD1F2D;">- «Сложное окно» в паспарту</span>– также отличный вариант сделать
+                    декоративное панно с футболкой интересным атрибутом интерьера.</p>
+
+            </div>
+        </div>
+
+
+        <div class="row text-center m-2">
+            <h3>ЗАЩИТА И ДОЛГОВЕЧНОСТЬ</h3>
+        </div>
+
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-8 my-auto">
+                <p style="font-weight: bold">Чтобы футболка сохранила свой вид на долгие годы, важно защитить ее от
+                    внешних воздействий:</p>
+                <p><span style="color: #AD1F2D;">- Багетное стекло:</span> используется для защиты от пыли и влаги.
+                    Рекомендуется выбирать антибликовое или УФ-защитное стекло</p>
+                <p><span style="color: #AD1F2D;">- Правильное размещение:</span>Избегайте прямого солнечного света и
+                    повышенной влажности, чтобы сохранить яркость цветов.</p>
+            </div>
+
+            <div class="col-12 col-md-4 text-center">
+                <img src="/img/article/IMG_8207.jpeg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+
+        </div>
+
+        <div class="row m-2 text-center">
+            <p style="font-weight: bold">
+                Багетная рама — это основа всего оформления. В Багетной мастерской №1 Вам предложат несколько вариантов
+                Оформление спортивных футболок в рамы — это отличный способ сохранить памятные моменты и добавить индивидуальности вашему интерьеру. Багетная мастерская №1 предлагает профессиональные услуги объемного оформления, которые помогут превратить ваше джерси в настоящий арт-объект. Обратитесь к специалистам, чтобы получить качественный результат, который будет радовать
+                вас долгие годы!
             </p>
+        </div>
+        <div class="row text-center mt-5">
+            <h4>С уважением к Вам,</h4>
+            <h4>С любовью к Искусству! </h4>
+            <h4>Команда Багетной мастерской №1</h4>
         </div>
     </div>
 
+
+    <style>
+
+        @media screen and (max-width: 1200px) {
+            .castom-image {
+                width: 180px !important;
+                height: 180px !important;
+            }
+        }
+
+        @media screen and (max-width: 900px) {
+            .castom-image {
+                width: 170px !important;
+                height: 170px !important;
+            }
+        }
+
+        @media screen and (max-width: 760px) {
+            .castom-image {
+                width: 130px !important;
+                height: 130px !important;
+            }
+
+            .card-body button {
+                font-size: 14px;
+            }
+
+            .card-body div.row a {
+                padding-left: 0px;
+                margin-left: 0px;
+            }
+        }
+
+        .castom-image {
+            width: 100% !important;
+            height: auto !important;
+            max-height: 170px;
+            object-fit: cover;
+        }
+
+        .card {
+            padding-bottom: 10px;
+            border-radius: 6px;
+            border: 3px solid var(--beige, #E0D2BB);
+            height: 350px !important;
+        }
+
+        .card:hover {
+            background: #E0D2BB;
+        }
+
+        p {
+
+            color: #474A51;
+            font-family: Manrope;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 400;
+            padding-bottom: 15px;
+
+        }
+
+        .show-mobile {
+            display: none;
+        }
+
+        h3 {
+            font-family: Cormorant Garamond;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+        }
+    </style>
 <?
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/section/desktop/vk.php';
