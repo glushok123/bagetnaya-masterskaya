3d50706688d95c1a96eb933970b247884ae0bff6
glushok <glushok19999@gmail.com>
2025-03-03 19:42:48 +0300

    правки

--- object_forming.php
+++ object_forming.php
@@ -1,80 +1,327 @@
 <?php
-$keywords = "Объект в рамке, предмет интерьера, украсить интерьер";
-$title = "Чем украсить интерьер? Оформляйте объекты в рамке с Багетной мастерской №1 !";
-$description = "Создать предмет интерьера легко! Сухоцветы, ракушки, монеты, украшения или слепки: подскажем наилучший вариант специально для Вас!";
+$keywords = "Глубокая рамка, предмет интерьера, оформить в рамку, объекты в рамке";
+$title = "Объекты в рамках как предмет интерьера! Воплощаем в реальность любые идеи!";
+$description = "Оформить в рамку объемные вещи легко с командой Багетной мастерской №1";
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 ?>
-    <div class="container text-center">
-    <h1>Объектное оформление</h1>
-        <hr>
+    <div class="container">
+        <div class="row text-center mt-3 mb-5">
+            <h1 class="color-main block-h1">ОБЪЕКТЫ В РАМКЕ КАК ПРЕДМЕТ ИНТЕРЬЕРА В БАГЕТНОЙ МАСТЕРСКОЙ №1</h1>
+        </div>
 
-    <div id="main">
-        <h1>Объектное оформление как эффектный предмет интерьера</h1>
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-4 text-center hide-mobile">
+                <img src="/img/article/IMG_0247.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
 
-        <img src="/img/photo_2023-05-15_13-26-09.jpg" align="left" width="200px">
-        <p>
-            Чем украсить свой дом? Или офисное пространство? Каждый, кто любит необычные и индивидуальные вещи,
-            задавался этим вопросом.
-            Ценный человеку объект в рамке – это отличный способ разнообразить пространство и сохранить жизнь тому или
-            иному предмету.
-            Это могут быть медали или украшения родных, привезенные из путешествия ракушки, сухоцветы, декоративные
-            маски, детские слепки,
-            спортивные атрибуты, монеты и многое другое!
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p style="padding-bottom: 0; margin-bottom: 5px;"
+                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
+                    Оформление объемных предметов в багетные рамы — это
+                    увлекательный способ превратить обычные вещи в настоящие
+                    произведения искусства. Такие композиции могут стать
+                    оригинальным украшением интерьера, памятным подарком
+                    или способом сохранить важные моменты жизни. Способ
+                    оформления, о котором мы сегодня поговорим, может иметь
+                    несколько названий: 3D-оформление, «аквариум», объемное
+                    или объектное оформление. В этой статье мы расскажем, как
+                    красиво и стильно оформить объекты в рамки.
+
+                </p>
+            </div>
+        </div>
+
+        <div class="row text-center mt-5 mb-2">
+            <h3>ВЫБОР ПРЕДМЕТА ДЛЯ ОФОРМЛЕНИЯ В ГЛУБОКИЕ РАМКИ</h3>
+            <p class="block-p-1">Объемные предметы, которые можно оформить в рамку, могут быть самыми разными:</p>
+        </div>
+
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-8 my-auto">
+                <p class="block-p-1"><span style="color: #AD1F2D;">- Природные элементы:</span>ракушки, камни, сухие цветы, веточки, листья.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">- Личные вещи:</span>украшения, ключи, часы, детские игрушки, пуговицы.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">- Спортивные атрибуты:</span>медали, значки, мячи, перчатки.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">- Тематические предметы:</span>кофейные зерна, специи, монеты, марки.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">- Творческие объекты:</span>миниатюрные скульптуры, фигурки, декоративные элементы.</p>
+            </div>
+
+            <div class="col-12 col-md-4 text-center">
+                <img src="/img/article/IMG_6198.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+
+        </div>
+        <p class="block-p-1 text-center"  style="font-weight: bold">Главное — выбрать предмет, который имеет для вас значение или просто радует глаз.
+        </p>
+        <div class="row text-center justify-content-center mt-3 mb-5">
+            <a href="/сatalog-of-finished-works">
+                <button
+                        class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
+                    Галерея работ</b></button>
+            </a>
+        </div>
+
+        <div class="row text-center mt-5 mb-2">
+            <h3>ВЫБОР БАГЕТНОЙ РАМЫ</h3>
+        </div>
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-4 text-center hide-mobile">
+                <img src="/img/article/IMG_0596.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p style="padding-bottom: 0; margin-bottom: 5px;"
+                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
+                    Рамка для объемного предмета должна быть глубокой
+                    (профиль «коробочка»). Такие рамки имеют достаточную
+                    глубину, чтобы вместить предмет интерьера и создать эффект
+                    "окна". Чтобы результат получился максимально качественный
+                    и подходящий к интерьеру – обращайтесь в Багетную
+                    мастерскую №1: наши специалисты имеют обширный опыт в
+                    данном способе оформления!
+
+
+                </p>
+            </div>
+        </div>
+
+        <div class="row text-center mt-5 mb-2">
+            <h3>ПОДГОТОВКА ФОНА</h3>
+            <p class="block-p-1">Фон играет важную роль в создании гармоничной композиции. Вот несколько идей:</p>
+        </div>
+
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-3 text-center hide-mobile">
+                <img src="/img/article/IMG_9225%20(1).jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+            <div class="col-12 col-md-6 my-auto">
+                <div class="font-20 color-red text-center">Монохромный фон:</div>
+                <div class="font-20 text-center">используйте однотонную бумагу, ткань или краску.</div>
+
+                <div class="font-20 color-red text-center">Текстурированный фон: </div>
+                <div class="font-20 text-center">добавьте фактуру с помощью мешковины, дерева или обоев.</div>
+
+                <div class="font-20 color-red text-center">Тематический фон:</div>
+                <div class="font-20 text-center">например, карта для путешествий, нотная бумага для
+                    музыкальных предметов или газетная вырезка для винтажного
+                    стиля</div>
+
+                <div class="font-20 color-red text-center">Минимализм: </div>
+                <div class="font-20 text-center">оставьте фон белым или прозрачным, чтобы акцент был на
+                    предмете.</div>
+            </div>
+            <div class="col-12 col-md-3 text-center hide-mobile">
+                <img src="/img/article/IMG_4659.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+        </div>
+        <p class="block-p-1 text-center"  style="font-weight: bold">В Багетной мастерской №1 Вам предложат широкую палитру вариантов из классических или текстурированных
+            паспарту для гармоничного расположения предметов интерьера!
         </p>
 
+        <div class="row text-center justify-content-center mt-3 mb-5">
 
-        <p>
-            Команда Багетной мастерской №1 превосходно владеет навыками создания объектного оформления! Оформление
-            телевизоров,
-            ракушек, шайб… Опыт «Багетной мастерской №1» в создании предметов интерьера очень обширный! Посмотреть
-            примеры Вы можете
-            в галерее наших работ, например:
-            <a href="https://bagetnaya-masterskaya.com/сatalog-of-finished-works-by-category.php?category=Зеркала%20и%20тв-панели">зеркала
-                и тв-панели, </a>
-            <a href="https://bagetnaya-masterskaya.com/сatalog-of-finished-works-by-category.php?category=Ордена%20и%20медали,%20купюры%20и%20монеты">ордена
-                и медали, </a>
-            <a href="https://bagetnaya-masterskaya.com/сatalog-of-finished-works-by-category.php?category=Ордена%20и%20медали,%20купюры%20и%20монеты">купюры
-                и монеты, </a>
+            <button
+                    class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'
+                    data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                Оставить заявку на обратную связь</b></button>
 
-            <a href="#">объектное оформление</a>.
+        </div>
 
-            Один из самых запоминающихся случаев: оформление крокодила в объемный деревянный короб! Найти фотографию
-            можно здесь:
-            <a href="https://bagetnaya-masterskaya.com/сatalog-of-finished-works-by-category.php?category=Сложные%20работы">сложные
-                работы </a>.
-        </p>
+        <div class="row text-center mt-5 mb-2">
+            <h3>ДЕКОР И АКЦЕНТЫ</h3>
+        </div>
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-8 my-auto">
+                <p>Добавьте детали, которые сделают композицию завершенной:</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Надписи:</span>подпишите предмет, добавьте дату или цитату при расположении в
+                    глубокой рамке</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Подсветка:</span>используйте светодиодные ленты для создания эффекта свечения.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Дополнительные элементы:</span>например, вокруг ключей можно добавить миниатюрные
+                    замки, а вокруг цветов — бабочек или стрекоз.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Цветовые акценты:</span>подберите рамку и фон в тон предмету или создайте контраст.</p>
+            </div>
 
-        <h5>Как создается оформление объекта в рамке </h5>
+            <div class="col-12 col-md-4 text-center">
+                <img src="/img/article/IMG_0109%20(1).jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+
+        </div>
+
+        <div class="row text-center justify-content-center mt-3 mb-5">
+            <a href="/baget_online">
+                <button
+                        class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
+                    Рассчитать стоимость багета</b></button>
+            </a>
+        </div>
+
+        <div class="row text-center mt-5 mb-2">
+            <h3>ИДЕИ ДЛЯ ОФОРМЛЕНИЯ</h3>
+        </div>
+
+        <div class="row m-3 flex-4">
+            <div class="col-12 col-md-4 text-center hide-mobile">
+                <img src="/img/article/IMG_9893.jpg" style="
+                        object-fit: cover;
+                        height: auto;
+                        width: 100%;
+                        margin-left: -15px;
+                        border-radius: 20px;
+                    ">
+
+            </div>
+            <div class="col-12 col-md-8 my-auto">
+                <p>Вот несколько вдохновляющих идей:</p>
+
+                <p class="block-p-1"><span style="color: #AD1F2D;">Морская тема: </span>ракушки, песок, морские звезды и синий фон.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Воспоминания о путешествиях:</span> билеты, карты, фотографии и сувениры.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Семейные ценности:</span>детские рисунки, первые зубы, локоны волос</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Спорт и хобби:</span>медали, ленты, инструменты или кисти.</p>
+                <p class="block-p-1"><span style="color: #AD1F2D;">Сезонные композиции:</span>осенние листья, елочные игрушки, весенние цветы.</p>
+
+            </div>
+        </div>
+
+        <div class="row text-center mt-5 mb-2">
+            <h3>СОВЕТЫ ПО РАЗМЕЩЕНИЮ</h3>
+        </div>
+
+        <div class="font-20 text-center">- Готовую рамку можно повесить на стену, поставить на полку или использовать как настольный декор.</div>
+        <div class="font-20 text-center">- Сочетайте несколько рамок в одном стиле для создания галереи.</div>
+        <div class="font-20 text-center">- Учитывайте стиль интерьера: минимализм, классика, прованс или лофт</div>
+        <div class="font-20 text-center mt-3">Оформить в рамки объемные предметы — это не только способ сохранить воспоминания, но и возможность проявить
+            творчество. Следуя этим советам, вы сможете создать уникальные композиции, которые станут изюминкой вашего
+            дома или прекрасным подарком для близких. Экспериментируйте, вдохновляйтесь и творите с удовольствием!.</div>
 
 
-        <img src="/img/photo_2023-05-15_13-26-27.jpg" align="right" width="200px">
-
-        <p>
-            Объектное оформление требует не только богатой фантазии дизайнера, но и высокого профессионализма багетного
-            мастера.
-            Для каждого объекта техника создания индивидуальна: необходимы точные замеры объекта, подбор материалов, а
-            также гораздо
-            большее время на изготовление. Чтобы украсить интерьер действительно красивой вещью, мы рекомендуем принести
-            объект в нашу
-            мастерскую, чтобы специалисты вживую ознакомились с будущим объектом в рамке.
-        </p>
-
-
-        <p>
-            Вы можете оставить предмет в нашей мастерской: так багетный мастер сможет не один раз возвращаться к объекту
-            и
-            перепроверять данные. Если же Вы не хотите оставлять предмет в мастерской, можно принести его «на примерку»:
-            мы снимем мерки, после чего Вы принесете его уже в заранее оговоренный день для монтажа!
-        </p>
-
-        <p>
-            Если Вам нравится идея объектного оформления, но Вы сомневаетесь – оставляйте заявку! Наши дизайнеры
-            свяжутся с Вами и предложат лучшие варианты!
-        </p>
+        <div class="row text-center mt-5">
+            <h4>С уважением к Вам,</h4>
+            <h4>С любовью к Искусству! </h4>
+            <h4>Команда Багетной мастерской №1</h4>
+        </div>
     </div>
-    </div>
 
+
+
+    <style>
+
+        @media screen and (max-width: 1200px) {
+            .castom-image {
+                width: 180px !important;
+                height: 180px !important;
+            }
+        }
+
+        @media screen and (max-width: 900px) {
+            .castom-image {
+                width: 170px !important;
+                height: 170px !important;
+            }
+        }
+
+        @media screen and (max-width: 760px) {
+            .castom-image {
+                width: 130px !important;
+                height: 130px !important;
+            }
+
+            .card-body button {
+                font-size: 14px;
+            }
+
+            .card-body div.row a {
+                padding-left: 0px;
+                margin-left: 0px;
+            }
+        }
+
+        .castom-image {
+            width: 100% !important;
+            height: auto !important;
+            max-height: 170px;
+            object-fit: cover;
+        }
+
+        .card {
+            padding-bottom: 10px;
+            border-radius: 6px;
+            border: 3px solid var(--beige, #E0D2BB);
+            height: 350px !important;
+        }
+
+        .card:hover {
+            background: #E0D2BB;
+        }
+
+        p {
+
+            color: #474A51;
+            font-family: Manrope;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 400;
+            padding-bottom: 15px;
+
+        }
+        .show-mobile{
+            display: none;
+        }
+        h3{
+            font-family: Cormorant Garamond;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+        }
+        .block-p-1{
+            margin: 0;
+        }
+        .color-red{
+            color: #AD1F2D;
+        }
+        .font-20{
+            font-size: 20px;
+        }
+    </style>
 <?
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/section/desktop/vk.php';
