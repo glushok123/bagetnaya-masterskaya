54fb077b58a4dda1ec8bda97e45620a7e6995fc2
glushok <glushok19999@gmail.com>
2025-06-20 15:13:59 +0300

    правки

--- contacts.php
+++ contacts.php
@@ -6,100 +6,136 @@ $description = "Контакты багетной мастерской в Мос
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 ?>
 
-    <div class="block-h1 text-center my-4 fade-in">
-        <h1 class='color-main'>КОНТАКТЫ</h1>
-    </div>
+    <style>
+        .section-title {
+            font-family: Cormorant Garamond;
+            color: #C6110F;
+            font-weight: bold;
+            text-align: center;
+            margin: 40px 0 20px;
+            text-transform: uppercase;
+            font-size: 50px;
+        }
+    </style>
+    <div class="container my-5">
+        <h1 class="text-center fw-bold text-uppercase mb-4 section-title" style="color: #921b1f;">Контакты</h1>
+        <p class="text-center mb-5" style="font-size: 25px">
+            В самом сердце Москвы – салоны Багетной мастерской №1 расположены <br class="d-none d-md-inline">
+            в шаговой доступности от метро, а также городскими и частными парковками!
+        </p>
 
-    <div class="container">
-        <hr>
-        <div class="row">
-            <div class="col">
-                <div style="text-align:left;" itemscope itemtype="http://schema.org/LocalBusiness">
-                    <h4 style="color: #6a1a21;" class="text-center"><strong itemprop="name">м.
-                            Новокузнецкая</strong></h4>
-                    <hr>
-                    <p><strong>Предоплата услуг: <a href="/oplata_uslug.php" class="t2">онлайн платеж</a></strong></p>
-                    <p><strong>Электронная почта:</strong> <a href="mailto:manager@bagetnaya-masterskaya.com"
-                                                              itemprop="email" class="t3">
-                            manager@bagetnaya-masterskaya.com</a></p>
-                    <p>
-                        <strong>Телефоны: </strong><!--a href="tel:+74997148062" itemprop="telephone" class="t3">8 (499)
-                            714-80-62</a-->, <a href="tel:+79778244212" itemprop="telephone" class="t3">8 (977) 824-42-12
-                            (WhatsApp)</a>
-                        <a title="Whatsapp" href="https://wa.me/79778244212"><img src="img/Whatsapp_37229.png"
-                                                                                  alt="Написать в Whatsapp"
-                                                                                  style="width:50px; height:50px;"/></a>
-                    </p>
-                    <p><strong>Время работы: </strong><span itemprop="openingHours" content="Mo-Su 9:00−21:00">9:00 - 21:00, ежедневно</span>
-                    </p>
-                    <p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><strong>Адрес:</strong>
-                        <span itemprop="addressLocality">Москва</span>, м. Новокузнецкая, <span
-                                itemprop="streetAddress">Климентовский переулок, 6</span></p>
-                </div>
-                <div class="yamap">
-                    <script type="text/javascript" charset="utf-8"
-                            src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=XD1WoHwePr9BAYkPBJ0XKFzAScY0NCvr&height=300"></script>
-                </div>
-            </div>
-            <div class="col">
-                <div id="arbat" style="text-align:left;" itemscope itemtype="http://schema.org/LocalBusiness">
-                    <h4 style="color: #6a1a21;" class="text-center"><strong itemprop="name">м.
-                            Арбатская</strong></h4>
-                    <hr>
-                    <p><strong>Предоплата услуг: <a href="/oplata_uslug.php" class="t2">онлайн платеж</a></strong></p>
-                    <p><strong>Электронная почта: </strong> <a href="mailto:manager@bagetnaya-masterskaya.com"
-                                                               itemprop="email" class="t3">
-                            manager@bagetnaya-masterskaya.com</a></p>
-                    <p>
-                        <strong>Телефоны: </strong><a href="tel:>+74956652561" itemprop="telephone" class="t3">8 (495)
-                            665-25-61, </a><a href="tel:>+79268659295" itemprop="telephone" class="t3">8 (926) 865-92-95
-                            (WhatsApp)</a>
-                        <a title="Whatsapp" href="https://wa.me/79268659295"><img src="img/Whatsapp_37229.png"
-                                                                                  alt="Написать в Whatsapp"
-                                                                                  style="width:50px; height:50px;"/></a>
-                    </p>
-                    <p><strong>Время работы: </strong><span itemprop="openingHours" content="Mo-Su 9:00−21:00">9:00 - 21:00, ежедневно</span>
-                    </p>
+        <!-- Версия для десктопа -->
+        <div class="d-none d-md-block">
+            <table class="table border align-middle">
+                <thead>
+                <tr class="text-uppercase  text-center" style="color: #AD1F2D;font-family: Cormorant Garamond;">
+                    <th>м.Новокузнецкая</th>
+                    <th>м.Арбатская</th>
+                    <th>м.Баррикадная</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <!-- Новокузнецкая -->
+                    <td>
+                        <div class="d-inline-block px-2 py-1 mb-2 fw-semibold mt-2" style="background-color: #E1D1BC;">
+                            Режим работы: 9:00–21:00, ежедневно
+                        </div>
+                        <p class="mb-3 mt-3"><b>Адрес:</b> Москва, Климентовский переулок, 6</p>
+                        <div class="mb-2">
+                            <iframe src="https://yandex.ru/map-widget/v1/?ll=37.626978%2C55.741455&z=16&pt=37.626978,55.741455,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                        </div>
+                        <p class="text-center mx-auto"><a href="tel:+79778244212" class="text-dark " style="text-decoration: underline;">8 (977) 824-42-12</a></p>
+                    </td>
 
+                    <!-- Арбатская -->
+                    <td>
+                        <div class="d-inline-block px-2 py-1 mb-2 fw-semibold mt-2" style="background-color: #E1D1BC;">
+                            Режим работы: 9:00–21:00, ежедневно
+                        </div>
+                        <p class="mb-3 mt-3"><b>Адрес:</b> Москва, ул. Арбат д. 1</p>
+                        <div class="mb-2">
+                            <iframe src="https://yandex.ru/map-widget/v1/?ll=37.600849%2C55.752041&z=16&pt=37.600849,55.752041,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                        </div>
+                        <p class="text-center mx-auto"><a href="tel:+79268659295" class="text-dark " style="text-decoration: underline;">8 (926) 865-92-95</a></p>
+                    </td>
 
-                    <p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><strong>Адрес:</strong>
-                        <span itemprop="addressLocality">Москва</span>, м. Арбатская, <span itemprop="streetAddress">ул. Арбат д. 1</span>
-                    </p>
-                </div>
-                <div class="yamap">
-                    <script type="text/javascript" charset="utf-8" async
-                            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7745ecc6090941daf7594769aab7ea687460dd4f16b9f44b54397c476ce3bfc8&amp;width=100%25&amp;height=300&amp;lang=ru_RU&amp;scroll=true"></script>
-                </div>
-            </div>
-            <div class="col">
-                <div id="arbat" style="text-align:left;" itemscope itemtype="http://schema.org/LocalBusiness">
-                    <h4 style="color: #6a1a21;" class="text-center"><strong itemprop="name">м.
-                            Баррикадная </strong></h4>
-                    <hr>
-                    <p><strong>Предоплата услуг: <a href="/oplata_uslug.php" class="t2">онлайн платеж</a></strong></p>
-                    <p><strong>Электронная почта: </strong> <a href="mailto:manager@bagetnaya-masterskaya.com"
-                                                               itemprop="email" class="t3">
-                            manager@bagetnaya-masterskaya.com</a></p>
-                    <p>
-                        <strong>Телефоны: </strong><a href="tel:>+79773147771" itemprop="telephone" class="t3">8(977)314-77-71
-                        <a title="Whatsapp" href="https://wa.me/79268659295"><img src="img/Whatsapp_37229.png"
-                                                                                  alt="Написать в Whatsapp"
-                                                                                  style="width:50px; height:50px;"/></a>
-                    </p>
-                    <p><strong>Время работы: </strong><span itemprop="openingHours" content="Mo-Su 9:00−21:00">9:00 - 21:00, ежедневно</span>
-                    </p>
+                    <!-- Баррикадная -->
+                    <td>
+                        <div class="d-inline-block px-2 py-1 mb-2 fw-semibold mt-2" style="background-color: #E1D1BC;">
+                            Режим работы: 9:00–21:00, ежедневно
+                        </div>
+                        <p class="mb-3 mt-3"><b>Адрес:</b> Москва, Баррикадная 21/34с3</p>
+                        <div class="mb-2">
+                            <iframe src="https://yandex.ru/map-widget/v1/?ll=37.579137%2C55.760506&z=16&pt=37.579137,55.760506,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                        </div>
+                        <p class="text-center mx-auto"><a href="tel:+79773147771" class="text-dark " style="text-decoration: underline;">8 (977) 314-77-71</a></p>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
 
-
-                    <p itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><strong>Адрес:</strong>
-                        <span itemprop="addressLocality">Москва</span>, <span itemprop="streetAddress">Баррикадная 21/34с3 </span>
-                    </p>
+        <!-- Версия для мобильных -->
+        <div class="d-md-none">
+            <div class="row row-cols-1 g-4 text-center">
+                <!-- Повторим каждую карточку как отдельный .col -->
+                <div class="col border p-3">
+                    <h5 class="text-uppercase fw-bold mb-3" style="color: #AD1F2D;">м.Новокузнецкая</h5>
+                    <div class="d-inline-block px-3 py-2 mb-2 fw-semibold" style="background-color: #E1D1BC;">
+                        Режим работы: 9:00–21:00, ежедневно
+                    </div>
+                    <p class="mb-2">Адрес: Москва, Климентовский переулок, 6</p>
+                    <iframe src="https://yandex.ru/map-widget/v1/?ll=37.626978%2C55.741455&z=16&pt=37.626978,55.741455,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                    <p class="fw-semibold mt-2">8 (977) 824-42-12</p>
                 </div>
-                <div class="yamap">
-                    <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/navi/213/moscow/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Москва</a><a href="https://yandex.ru/navi/213/moscow/?ll=37.585199%2C55.759776&mode=whatshere&utm_medium=mapframe&utm_source=maps&whatshere%5Bpoint%5D=37.579137%2C55.760506&whatshere%5Bzoom%5D=20&z=15.8" style="color:#eee;font-size:12px;position:absolute;top:14px;">Баррикадная улица, 21/34с3 — Яндекс Карты</a><iframe src="https://yandex.ru/map-widget/v1/?ll=37.585199%2C55.759776&mode=whatshere&whatshere%5Bpoint%5D=37.579137%2C55.760506&whatshere%5Bzoom%5D=20&z=15.8" width="100%" height="300" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
+
+                <div class="col border p-3">
+                    <h5 class="text-uppercase fw-bold mb-3" style="color: #AD1F2D;">м.Арбатская</h5>
+                    <div class="d-inline-block px-3 py-2 mb-2 fw-semibold" style="background-color: #E1D1BC;">
+                        Режим работы: 9:00–21:00, ежедневно
+                    </div>
+                    <p class="mb-2">Адрес: Москва, ул. Арбат д. 1</p>
+                    <iframe src="https://yandex.ru/map-widget/v1/?ll=37.600849%2C55.752041&z=16&pt=37.600849,55.752041,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                    <p class="fw-semibold mt-2">8 (926) 865-92-95</p>
+                </div>
+
+                <div class="col border p-3">
+                    <h5 class="text-uppercase fw-bold mb-3" style="color: #AD1F2D;">м.Баррикадная</h5>
+                    <div class="d-inline-block px-3 py-2 mb-2 fw-semibold" style="background-color: #E1D1BC;">
+                        Режим работы: 9:00–21:00, ежедневно
+                    </div>
+                    <p class="mb-2">Адрес: Москва, Баррикадная 21/34с3</p>
+                    <iframe src="https://yandex.ru/map-widget/v1/?ll=37.579137%2C55.760506&z=16&pt=37.579137,55.760506,pm2rdm" width="100%" height="180" frameborder="0"></iframe>
+                    <p class="fw-semibold mt-2">8 (977) 314-77-71</p>
                 </div>
             </div>
         </div>
-    </div>
+
+        <!-- Нижний блок -->
+        <div class="row mt-5 align-items-start">
+            <div class="col-2 text-start d-none d-md-block">
+                <!-- Картинка "1" -->
+                <img src="/Group%2048095855%20(1).png" alt="decorative icon" class="img-fluid" style="max-width: 40px;">
+            </div>
+            <div class="col-12 col-md-8 mb-8 mb-md-0 text-md-start text-center">
+                <p class=" mb-0" style="font-size: 25px">
+                    Выбирайте удобный для Вас салон и создавайте красоту вместе<br>
+                    <strong>с нашей командой!</strong>
+                </p>
+            </div>
+
+        </div>
+
+
+        <div class="col-12 col-md-4 text-md-end text-center mt-5 " style="width: 100%">
+            <p class="mb-0" style="font-size: 25px">
+                С уважением к Вам,<br>
+                С любовью к Искусству!<br>
+                <strong>Багетная мастерская №1</strong>
+            </p>
+        </div>
+
 
 
 <?
