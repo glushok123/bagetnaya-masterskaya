bcc0a59ad54143ac89c2f67bd2f5916a6c6e05c8
glushok <glushok19999@gmail.com>
2025-07-25 11:22:20 +0300

    правки

--- template/layout/footer.php
+++ template/layout/footer.php
@@ -2,13 +2,13 @@
     <footer class="">
         <div class="row">
             <div class="col-12 col-md-7 align-middle">
-                <a class="navbar-brand align-middle" href="/">Багетная мастерская</a>
+                <a class="navbar-brand align-middle" href="/">Багетная мастерская №1</a>
                 <img src="/img/logo 2 (1).svg" alt="" class='img-brand-footer align-middle'>
             </div>
             <div class="col-12 col-md-5">
                 <div class='row
                     <? if (!isMobile()) { ?>
-                        text-end
+
                     <? } ?>
                     mob-pt-15'>
 
