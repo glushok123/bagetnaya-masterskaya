739e101e0489f51e804c1cef622689364376ce3c
glushok <glushok19999@gmail.com>
2025-09-25 14:48:05 +0300

    правки

--- template/layout/header.php
+++ template/layout/header.php
@@ -12,7 +12,7 @@ header("Cache-Control: max-age=2592000");
 require_once $_SERVER['DOCUMENT_ROOT'] . '/block-bots.php';
 $showMetrika = blockBot();
 
-$v = 14;
+$v = 16;
 ?>
     <!doctype html>
     <html lang="ru">
