246e3a70a71d63b9e6ef06ac2f8e49a7526a0135
glushok <glushok19999@gmail.com>
2025-10-22 17:19:21 +0300

    правки

--- template/layout/footer.php
+++ template/layout/footer.php
@@ -513,8 +513,32 @@
             clear: () => { trailSettings.mode==='line' ? clearLine() : pool.splice(0).forEach(el=>el.remove()); }
         };
     });
+
+
 </script>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t), a=e.getElementsByTagName(t)[0],
+            k.async=1; k.src=r; a.parentNode.insertBefore(k,a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(60934651, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript>
+    <div>
+        <img src="https://mc.yandex.ru/watch/60934651" style="position:absolute; left:-9999px;" alt="">
+    </div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
 
 
 </body>
