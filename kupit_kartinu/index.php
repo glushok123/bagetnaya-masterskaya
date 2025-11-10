70321bc7ff5a95f1de025f98552211ca9c84f10e
glushok <glushok19999@gmail.com>
2025-11-10 15:50:35 +0300

    правки

--- kupit_kartinu/index.php
+++ kupit_kartinu/index.php
@@ -5,71 +5,80 @@ $description = "Купить картину художника, постер и�
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 ?>
-    <div class="container">
-        <div class="row text-center mt-3 mb-4">
-            <h1 class="color-main block-h1">ПРОДАЖА ИНТЕРЬЕРНЫХ КАРТИН – ОРИГИНАЛЫ И РЕПРОДУКЦИИ</h1>
-        </div>
+<div class="container">
+    <div class="row text-center mt-3 mb-4">
+        <h1 class="color-main block-h1">ОРИГИНАЛЫ И РЕПРОДУКЦИИ<br>
+            ГОТОВЫЕ ИНТЕРЬЕРНЫЕ КАРТИНЫ, ДОСТУПНЫЕ К ПОКУПКЕ
+        </h1>
+    </div>
 
-        <div class="row justify-content-center mb-4">
-            <div class="col-12 col-lg-10">
-                <p class="element-animation<? if (isMobile()) { ?> text-center<? } ?>">
-                    Купить картину художника или приобрести репродукцию – команда Багетной мастерской №1 предлагает Вам ознакомиться с ассортиментом разных картин, которые ищут своего счастливого обладателя! Все картины представлены в салонах наших мастерских, а некоторые из них доступны к повтору! Заказывайте онлайн с доставкой в подарок в пределах МКАД!
-                </p>
-            </div>
-        </div>
-
-        <div class="row text-center justify-content-center mt-3 mb-5">
-            <a href="/baget_online">
-                <button class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
-                    Рассчитать стоимость багетного оформления
-                </button>
-            </a>
-        </div>
-
-        <div class="row text-center m-2">
-            <h2 class="section-title">КАРТИНА ДЛЯ ДОМА ИЛИ В ПОДАРОК</h2>
-        </div>
-        <div class="row justify-content-center mb-4">
-            <div class="col-12 col-lg-10 text-center text-lg-start">
-                <p>
-                    Каждую картину Вы можете приобрести очно в салонах Багетной мастерской №1 или заказать с доставкой! Готовая картина – это идеальный вариант для подарка любителю искусства, а также великолепное решение для дома или офисного пространства. Не откладывайте на потом, приобретайте репродукции и оригиналы художников уже сегодня!
-                </p>
-            </div>
-        </div>
-
-        <div class="row text-center justify-content-center mt-3 mb-5">
-            <button
-                    class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'
-                    data-bs-toggle="modal" data-bs-target="#feedbackModal">
-                Получить консультацию
-            </button>
+    <div class="row justify-content-center mb-4">
+        <div class="col-12 col-lg-10">
+            <p class="element-animation<? if (isMobile()) { ?> text-center<? } ?>">
+                Багетная мастерская №1 – это место, где Вы можете не только заказать индивидуальное оформление для Ваших
+                работ, но также купить картину или репродукцию известного художника! Мы регулярно стараемся пополнять
+                ассортимент, а также сотрудничаем с молодыми художниками. Большую часть готовых работ Вы можете увидеть
+                в салонах на м. Арбатская и м. Баррикадная. Кроме того, Вы можете приобрести картину для дома или офиса
+                через сайт с бесплатной доставкой по г. Москва в проделах МКАД!
+            </p>
         </div>
     </div>
 
-    <style>
+    <div class="row text-center justify-content-center mt-3 mb-5">
+        <a href="/baget_online">
+            <button class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'>
+                Рассчитать стоимость багетного оформления
+            </button>
+        </a>
+    </div>
+
+    <div class="row text-center m-2">
+        <h2 class="section-title">КАРТИНА ДЛЯ ДОМА ИЛИ В ПОДАРОК</h2>
+    </div>
+    <div class="row justify-content-center mb-4">
+        <div class="col-12 col-lg-10 text-center text-lg-start">
+            <p>
+                Готовые картины – это также великолепный вариант подарка на любой праздник как для мужчин, так и для
+                женщин! Нужна консультация? Оставляйте заявку на обратную связь. Наши специалисты проконсультируют Вас и
+                ответят на все вопросы!
+            </p>
+            <p>Создавайте красоту и уют вместе с командой Багетной мастерской №1!</p>
+        </div>
+    </div>
+
+    <div class="row text-center justify-content-center mt-3 mb-5">
+        <button
+                class='button button-custom-index button-color-company-red fix-width-425 mob-fix-width-340 mb-3 color-white'
+                data-bs-toggle="modal" data-bs-target="#feedbackModal">
+            Получить консультацию
+        </button>
+    </div>
+</div>
+
+<style>
+    p {
+        color: #474A51;
+        font-family: Manrope;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 1.6;
+    }
+
+    .section-title {
+        font-family: Cormorant Garamond;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+    }
+
+    @media screen and (max-width: 760px) {
         p {
-            color: #474A51;
-            font-family: Manrope;
-            font-size: 20px;
-            font-style: normal;
-            font-weight: 400;
-            line-height: 1.6;
+            font-size: 18px;
+            text-align: center;
         }
-
-        .section-title {
-            font-family: Cormorant Garamond;
-            font-style: normal;
-            font-weight: 500;
-            line-height: normal;
-        }
-
-        @media screen and (max-width: 760px) {
-            p {
-                font-size: 18px;
-                text-align: center;
-            }
-        }
-    </style>
+    }
+</style>
 <?
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/section/desktop/vk.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/section/desktop/sm.php';
