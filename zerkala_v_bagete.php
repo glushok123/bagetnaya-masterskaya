d52e90ebcf640ff338f65f6b7f4bbbfd08959798
glushok <glushok19999@gmail.com>
2025-12-15 14:56:33 +0300

    правки

--- zerkala_v_bagete.php
+++ zerkala_v_bagete.php
@@ -1,25 +1,26 @@
 <?php
 $keywords = "Интерьерное зеркало, зеркало в раме, зеркало на заказ";
-$title = "Зеркала в рамах на заказ для любого интерьера!";
+$title = "Зеркало в раме – расширяем пространства!";
 $description = "Интерьерные зеркала под ключ в Багетной мастерской №1 ";
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 ?>
     <div class="container">
         <div class="row text-center mt-3 mb-5">
-            <h1 class="color-main block-h1">КАК И ЗАЧЕМ ОФОРМЛЯЮТ ЗЕРКАЛА В РАМЫ</h1>
+            <h1 class="color-main block-h1">ИНТЕРЬЕРНЫЕ ЗЕРКАЛА: КРАСОТА И ПРАКТИЧНОСТЬ В КАЖДОЙ ДЕТАЛИ</h1>
         </div>
 
         <div class="row m-3 text-center">
             <p>
-                Зеркала — это не только функциональный предмет интерьера, но и важный элемент декора, способный
-                преобразить любое пространство. Оформление зеркал в рамы — это популярная услуга, которая позволяет
-                создать уникальный акцент в интерьере. Рассмотрим, как происходит этот процесс и зачем он нужен.
+                Зеркало в раме - это популярный объект в каждом интерьере! Зеркальные полотна делают пространства
+                светлее, а также визуально расширяет помещение. Различные формы зеркал, а также широкий ассортимент
+                багета позволяет команде Багетной мастерской №1 создавать для Вас уникальные проекты! В данной статье
+                рассказываем, какие преимущества в индивидуальных проектах!
             </p>
         </div>
 
         <div class="row text-center mt-3 mb-5">
-            <h3>ЗАЧЕМ ОФОРМЛЯТЬ ЗЕРКАЛА В РАМЫ?</h3>
+            <h3>ЗЕРКАЛО НА ЗАКАЗ: ПРЕИМУЩЕСТВА</h3>
         </div>
         <div class="row m-3 flex-4">
             <div class="col-12 col-md-4 text-center">
@@ -35,40 +36,43 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
             <div class="col-12 col-md-8 my-auto">
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    1. Эстетика и стиль
+                    1. Правильный цвет и форма
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Зеркало на заказ становится полноценным и уникальным элементом декора. Оно может подчеркнуть стиль
-                    интерьера — будь то классика, модерн, лофт или минимализм. Рама добавляет зеркалу завершенность и
-                    делает его акцентом в помещении.
+                    Сколько интерьеров, столько и вариантов! От резных классических профилей до строго минимализма:
+                    сегодня зеркала можно оформить в очень узкие алюминиевые рамы для поддержания строгой стилистики,
+                    так и фигурные круглые или полукруглые багеты!
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    2. Индивидуальность
+                    2. Индивидуальный подход
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Зеркало с уникальным оформлением позволяет создать эксклюзивный предмет интерьера. Вы можете выбрать
-                    форму, размер, цвет и материал рамы, чтобы она идеально вписалась в ваш интерьер.
+                    Наша команда к каждому проекту относится с большим трепетом! Мы подготавливаем визуализации будущих
+                    зеркал в раме для наглядного понимания и простого согласования!
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    3. Защита краев зеркала
+                    3. Качество, долговечность и гарантия
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Рама не только украшает зеркало, но и защищает его края от сколов и повреждений. Это особенно важно
-                    для больших интерьерных зеркал, которые активно используются в повседневной жизни.
+                    Наша команда не только аккуратно выполнит Ваш проект! Мы предоставляем гарантию 1 год на все
+                    изделия, выполненные в наших салонах. Кроме того, Вы можете оформить доставку по любому удобному
+                    адресу в пределах Москвы и МО. Доставка в более удаленные города и регионы осуществляется с помощью
+                    транспортных компаний.
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    4. Эффект расширения пространства
+                    4. Удобное оформление заказа
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Большие зеркальные полотна также визуально увеличивают масштаб пространства, а также делает его
-                    светлее!
+                    Вы можете оформить заказ на зеркальное полотно в багетной раме любым удобным для Вас способом,
+                    например посетить любой салон Багетной мастерской №1 в Москве, самостоятельно оформить заказ в
+                    онлайн-конструкторе багета или обратитесь к менеджеру в удобном для Вас мессенджере!
                 </p>
             </div>
         </div>
@@ -83,7 +87,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 
 
         <div class="row text-center mt-3 mb-5">
-            <h3>ЧТО ВАЖНО В ОФОРМЛЕНИИ ЗЕРКАЛ В РАМЫ?</h3>
+            <h3>ЧИНТЕРЬЕРНЫЕ ЗЕРКАЛА: СТОИМОСТЬ И СРОКИ ИЗГОТОВЛЕНИЯ</h3>
         </div>
         <div class="row m-3 flex-4">
             <div class="col-12 col-md-4 text-center">
@@ -97,39 +101,34 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
 
             </div>
             <div class="col-12 col-md-8 my-auto">
-                <p style="font-weight: bold">Процесс оформления зеркала в Багетной мастерской №1 включает несколько
-                    этапов:</p>
+                <p style="font-weight: bold">В отличие от готовых зеркал на маркетплейсах и офлайн магазинов в
+                    индивидуальном подходе. Вы можете заказать любой формат и подходящий дизайн, а не подстраиваться под
+                    готовый вариант! Ценообразование сроки готовности зеркальных полотен зависит от нескольких
+                    факторов: </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    1. Выбор рамы
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    В Багетной мастерской №1 вам предложат широкий ассортимент рам из различных материалов: дерево,
-                    металл, пластик. Рама должна не только соответствовать стилю, но также быть достаточно массивной при
-                    большом формате
-                    зеркала.
+                    - размер полотна, наличие фацета, необычная форма
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    2. Подготовка зеркального полотна
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Если вы планируете разместить в своем пространстве зеркало на заказ, следует сразу сообщить мастерам
-                    площадь, которую должно занимать зеркало с учетом ширины багета! При изготовлении зеркал Багетная
-                    мастерская №1 предусматривает защитные пленки, обработанные края. Также фацет и гравировка при
-                    необходимости!
+                    - наличие багета, изготовление нестандартной формы, индивидуальный цвет рамы
                 </p>
                 <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    3. Установка креплений
+                    - наличие нестандартных функций: подсветка, мозаика и другое
                 </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
+                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
                    class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    В зависимости от того, как вы планируете разместить зеркало (на стене, на комоде или в другом
-                    месте), наши специалисты установят соответствующие
-                    крепления!
+                    - крупногабаритная доставка
+                </p>
+                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
+                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
+                    - уже смонтированное на стену зеркало в Вашем интерьере
+                </p>
+
+                <p>
+                    Также команда Багетной мастерской №1 может оформлять Ваши зеркала, если полотно уже имеется! Для
+                    уточнения деталей оставляйте заявку на обратную связь!
                 </p>
 
             </div>
@@ -145,89 +144,11 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
         </div>
 
 
-        <div class="row text-center mt-3 mb-5">
-            <h3>ПРЕИМУЩЕСТВА ОФОРМЛЕНИЯ ЗЕРКАЛ В
-                БАГЕТНОЙ МАСТЕРСКОЙ №1</h3>
-        </div>
-
-        <div class="my-auto">
-            <p style="padding-bottom: 0; margin-bottom: 5px;     "
-               class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                <span style="color: #AD1F2D;">Широкий выбор материалов и дизайнов:</span> Вы можете выбрать раму,
-                которая идеально подойдет под стиль вашего интерьера.
-            </p>
-            <p style="padding-bottom: 0; margin-bottom: 5px;     "
-               class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                <span style="color: #AD1F2D;">Индивидуальный подход: </span> Зеркало на заказ — это возможность получить
-                уникальный предмет, который отражает ваш вкус и предпочтения.
-            </p>
-            <p style="padding-bottom: 0; margin-bottom: 5px;     "
-               class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                <span style="color: #AD1F2D;">Качество и долговечность:</span> Профессиональное изготовление зеркала
-                гарантирует, что зеркало будет служить вам долгие годы, сохраняя свой первоначальный вид.
-            </p>
-
-        </div>
-
-        <div class="row text-center m-5">
-            <h3>ИДЕИ ДЛЯ ИСПОЛЬЗОВАНИЯ ЗЕРКАЛ В ИНТЕРЬЕРЕ</h3>
-        </div>
-
-        <div class="row m-3 flex-4">
-            <div class="col-12 col-md-4 text-center">
-                <img src="/img/article/IMG_0166.jpg" style="
-                        object-fit: cover;
-                        height: auto;
-                        width: 100%;
-                        margin-left: -15px;
-                        border-radius: 20px;
-                    ">
-
-            </div>
-            <div class="col-12 col-md-8 my-auto">
-                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    - Гостиная
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Большое интерьерное зеркало в раме может стать центральным элементом гостиной, визуально расширяя
-                    пространство и добавляя света.
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    - Спальня
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Зеркало в изящной раме над туалетным столиком или комодом создаст атмосферу уюта и элегантности.
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    - Прихожая
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Зеркало в прихожей не только поможет привести себя в порядок перед выходом, но и сделает помещение
-                    более стильным.
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;     color: #AD1F2D;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    - Ванная комната
-                </p>
-                <p style="padding-bottom: 0; margin-bottom: 5px;"
-                   class="element-animation <? if (isMobile()) { ?> text-center <? } ?>">
-                    Влагостойкие рамы для зеркал добавят изысканности ванной комнате.
-                </p>
-            </div>
-        </div>
-
         <div class="row m-5 text-center">
             <p>
-                Обрамление зеркал — это возможность превратить обычный предмет интерьера в произведение искусства.
-                Зеркало, изготовленное с индивидуальным подходом, станет не только функциональным элементом, но и ярким
-                акцентом, который подчеркнет ваш вкус и стиль. Обратившись в Багетную мастерскую №1, Вы получите
-                качественное и красивое решение, которое преобразит ваш дом.
+                Зеркало на заказ – это всегда красиво, практично и уникально! Оформляя заказ на индивидуальное
+                изготовление зеркала в багетной раме Вы можете быть уверены в наивысшем качестве, точных сроках
+                готовности и комфортных стоимостях!
             </p>
         </div>
 
@@ -310,7 +231,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template/layout/header.php';
             font-weight: 500;
             line-height: normal;
         }
-        .mob-fix-width-340{
+
+        .mob-fix-width-340 {
             width: auto;
         }
     </style>
