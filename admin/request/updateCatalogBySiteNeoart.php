135baf5f46b594693513c4b6e921f20e5c3a7980
glushok <glushok19999@gmail.com>
2026-05-08 14:39:02 +0300

    правки

--- admin/request/updateCatalogBySiteNeoart.php
+++ admin/request/updateCatalogBySiteNeoart.php
@@ -128,7 +128,7 @@ foreach ($xmlalum->category->item as $item) {
     }
 
     $countW[$s] = (int)$item->quantity->countse;
-    $price = round($priceW[$s] * 5);
+    $price = round($priceW[$s] * 6);
     $count = round($countW[$s]);
 
     $stm = $dbh->prepare("SELECT * FROM catalog_baget where vendor=?");
